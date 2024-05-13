737083b674858e60b68f2e1364d9add18b8f82bc
sujan khatri <sujansince2059@gmail.com>
2024-05-13 23:56:33 +0545

    configuring CORS Encoding URL uisng parser to set and get cookie in browser

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+//configuring CORS
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); //express is accepting json data with size upto 16kb
+
+// encoding URL
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+//using cookie parser package to store and access cookies of client browser from server
+
+app.use(cookieParser());
+
+export { app };
