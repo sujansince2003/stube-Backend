9d22808ad813176c42bd3b25f420fe2fc0d62909
sujan khatri <sujansince2059@gmail.com>
2024-05-13 23:55:30 +0545

    handling promises to connect to DB

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import mongoose from "mongoose";
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+//connecting to database
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server is running on port ${process.env.PORT} `);
+    });
+  })
+  .catch((err) => {
+    console.log("connection failed in promise", err);
+  });
