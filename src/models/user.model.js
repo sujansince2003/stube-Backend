a474200d1ee8a319d6975f99ee03427024e6a560
sujan khatri <sujansince2059@gmail.com>
2024-05-15 22:52:48 +0545

    defining userModel for users data

---
+++ src/models/user.model.js
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // makes searching more easier
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //stores url from cloud storage service
+      required: true,
+    },
+    coverImage: {
+      type: String, //stores url from cloud storage service
+      required: true,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+  },
+
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
