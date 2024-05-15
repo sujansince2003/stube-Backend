5fc53c29cbaca4a3ac4a286f04179186879ea78f
sujan khatri <sujansince2059@gmail.com>
2024-05-15 22:53:30 +0545

    async code handler

---
+++ src/utils/asyncHandle.js
@@ -0,0 +1,17 @@
+const asyncHandle = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandle };
+
+// const asyncHandle = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res
+//       .status(error.code || 500)
+//       .json({ success: false, message: error.message });
+//   }
+// };
