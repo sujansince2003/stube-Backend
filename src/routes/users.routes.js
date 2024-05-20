11cb09be841709a5e311c4f1e5794d0406426f08
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-05-20 19:14:20 +0545

    handling file uploads before calling registerUser controller

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-
+import { upload } from "../middlewares/multer.middlewares.js";
 const router = Router();
 
-router.route("/register").post(registerUser);
+//just before calling  registerUser we call multer upload middleware
+
+router.route("/register").post(
+  upload.fields([
+    { name: "avatar", maxCount: 1 }, //use same name in formfield in frontend
+    { name: "coverImage", maxCount: 1 },
+  ]),
+
+  registerUser
+);
 
 export default router;
